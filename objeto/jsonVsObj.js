802a38dc5af90d7ed090b05c12434623554b849e
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-04-29 16:26:20 -0300

    29/04/22

---
+++ objeto/jsonVsObj.js
@@ -0,0 +1,16 @@
+// JSON é textual
+const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    soma() {
+        return a + b + c;
+    },
+};
+console.log(JSON.stringify(obj));
+//console.log(JSON.parse("{ a: 1, b: 2, c: 3 }"));
+//console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }"));
+console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }'));
+console.log(
+    JSON.parse('{ "a": 1.5, "b": "string", "c": true, "d": {}, "e": [] }')
+);
