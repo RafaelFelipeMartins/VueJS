3186297e1afb71aa2989b8b6d1e3a5bc01335a66
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-02 11:08:48 -0300

    02/05/22

---
+++ array/forEach1.js
@@ -0,0 +1,11 @@
+const aprovados = ["Agatha", "Aldo", "Daniel", "Raquel"];
+
+aprovados.forEach(function (nome, indice) {
+    console.log(`(${indice + 1}) ${nome}`);
+    console.log(Array);
+});
+
+aprovados.forEach((nome) => console.log(nome));
+
+const exibirAprovados = (aprovado) => console.log(aprovado);
+aprovados.forEach(exibirAprovados);
