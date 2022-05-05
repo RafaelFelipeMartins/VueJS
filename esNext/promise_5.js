e1e8259db059041bfe0d00a0ddc78eaff326a5f3
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-05 14:12:21 -0300

    05/05/22

---
+++ esNext/promise_5.js
@@ -0,0 +1,24 @@
+function funcionarOuNao(valor, chanceDeErro) {
+    return new Promise((resolve, reject) => {
+        try {
+            con.log("temp");
+            if (Math.random() < chanceDeErro) {
+                reject("Ocorreu um erro!");
+            } else {
+                resolve(valor);
+            }
+        } catch (e) {
+            resolve(e);
+        }
+    });
+}
+
+funcionarOuNao("Testado...", 0.5)
+    .then((v) => console.log(`Valor: ${v}`))
+    .then(
+        (v) => console.log(v),
+        (err) => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log("Quase fim!"))
+    .catch((err) => console.log(`Erro: ${err}`))
+    .then(() => console.log("Fim!"));
