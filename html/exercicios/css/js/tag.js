b9c1f9e598e3e226e3528010073af1c38e4f0054
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-10 11:46:31 -0300

    10/05/22

--- html/exercicios/css/js/tag.js
+++ html/exercicios/css/js/tag.js
@@ -1,12 +1,31 @@
-document.querySelectorAll('.tag').forEach(box => {
-    const tagName = box.tagName.toLowerCase()
+const colors = {
+    p: '#388e3c',
+    div: '#1565c0',
+    span: '#e53935',
+    section: '#f67809',
+    ul: '#5e35b1',
+    ol: '#fbc02d',
+    header: '#d81b60',
+    nav: '#64dd17',
+    main: '#00acc1',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    padrao: '#616161',
+    get(tag) {
+        return this[tag] ? this[tag] : this.padrao 
+    }
+}
 
-    box.getElementsByClassName.borderColor = '#616161'
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLowerCase()
 
-    if (!box.classList.contains('nolabel')) {
+    elemento.style.borderColor = colors.get(tagName)
+
+    if (!elemento.classList.contains('nolabel')) {
         const label = document.createElement('label')
-        label.style.backgroundColor = '#616161'
+        label.style.backgroundColor = colors.get(tagName)
         label.innerHTML = tagName 
-        box.insertBefore(label, box.childNodes[0])
+        elemento.insertBefore(label, elemento.childNodes[0])
     }
 })
