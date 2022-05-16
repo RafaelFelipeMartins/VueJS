a94481bda213d76376d6df31acc212ab03bc3c8c
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-16 09:49:01 -0300

    16/05/22

--- ajax/server.js
+++ ajax/server.js
@@ -32,9 +32,19 @@ app.post('/upload', (req, res) => {
 app.post('/formulario', (req, res) => {
      res.send({
          ...req.body,
-         id: 1
+         id: 7
      })
 }) 
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
 
 /*app.get('/teste', (req, res) => res.send(new Date))*/
 app.listen(8080, () => console.log('Dança gatinho... Dança...'))
