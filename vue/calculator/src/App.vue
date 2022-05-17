b802697655a5e8c1c814577616dfcf438fa48e8f
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-17 15:46:17 -0300

    17/05/22

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <h1>Calculator</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+    components: { Calculator }
+}
+</script>
+
+<style>
+    @font-face {
+        font-family: "RobotoMono";
+        src: url("./fonts/RobotoMono-Thin.ttf");
+    }
+
+    * {
+        font-family: "RobotoMono", monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: #fff;
+        background: linear-gradient(to right, rgb(78, 70, 73), rgb(77, 28, 88));
+    }
+</style>
---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="calculator">
+        <Display />
+        <Button label="AC" triple @onCalcButtonClick="clearMemory"  />
+        <Button label="/" operation @onCalcButtonClick="setOperation"/>
+        <Button label="7" @onCalcButtonClick="addDigit"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="*" operation/>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="-" operation/>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+" operation/>
+        <Button label="0" double/>
+        <Button label="."/>
+        <Button label="=" operation/>
+
+    </div>
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+import Display from '../components/Display'
+
+
+export default {
+    components: { Button, Display, Display },
+    methods: {
+        clearMemory() {
+            console.log('Limpar Memória!')
+        },
+        setOperation(operation) {
+            console.log('Operação ' + operation)
+        },
+        addDigit(n) {
+            console.log('Digito ' + n) 
+        }
+    }
+}
+</script>
+
+<style>
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px;
+}
+</style>
