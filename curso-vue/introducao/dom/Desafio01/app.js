893116bd93c986a6fe0c85aaab39719f7a46916d
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-18 13:50:24 -0300

    18/05/22

---
+++ curso-vue/introducao/dom/Desafio01/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#desafio",
+    data: {
+        idade: '21',
+        imgLink : 'https://aventurasnahistoria.uol.com.br/media/_versions/arqueologia/crocodilo_do_nilo_widelg.jpg'
+    },
+    methods: {
+        idadeMult(){
+           const result = this.idade * 3
+           return result
+        },
+        random(min, max){
+            const num = Math.floor(Math.random() * 10)
+            return num
+        }
+    }
+})
