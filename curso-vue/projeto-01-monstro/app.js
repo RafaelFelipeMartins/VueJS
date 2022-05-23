61ee0d8a4e261da06b6257722f8498cdcdfae515
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-23 15:21:27 -0300

    23/05/22

---
+++ curso-vue/projeto-01-monstro/app.js
@@ -0,0 +1,38 @@
+new Vue({
+    el: '#app',
+    data: {
+        running: false,
+        playerLife: 100,
+        monsterLife: 100
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, false)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        hurt(prop ,min, max, especial) {
+            const plus = especial ? 5 : 0 
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() + (max - min) + min
+            return Math.round(value)
+        }
+    },
+    watch: {
+        hasResult(value) {
+            if(value) this.running = false
+        }
+    }
+})
