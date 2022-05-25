4659f8e27361f7eb2a9c9412fce7f3be0850549b
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-25 11:27:56 -0300

    25/05/22

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Botão</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        titulo: 'Tst data'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: blueviolet;
+    color: rgb(255, 255, 255);
+    margin: 0px;
+    padding: 10px;
+  }
+</style>
