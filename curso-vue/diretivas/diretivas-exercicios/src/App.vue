30bf62bb3f8e68e93904b3bca98c3ee4ded25f15
Robson Monteiro <57761831+Miopiaa@users.noreply.github.com>
2022-05-27 17:02:39 -0300

    27/05/22

--- curso-vue/diretivas/diretivas-exercicios/src/App.vue
+++ curso-vue/diretivas/diretivas-exercicios/src/App.vue
@@ -2,13 +2,55 @@
 	<div id="app">
 		<h1>Diretivas</h1>
 		<hr>
-		<p v-text="Usando diretiva v-text"></p>
+		<p v-text="'Usando diretiva v-text'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<p v-destaque:fundo="'blue'">Usando diretiva personalizada</p>
+		<p v-destaque="cor">Usando diretiva personalizada</p>
+		<p v-destaque.atrasar="cor">Usando diretivas personalizada</p>
+		<hr>
+		<p v-destaque-local:fundo.atrasar="'blue'">Usando diretiva personalizada</p>
+		<p v-destaque-local.atrasar.alternar="cor">Usando diretiva personalizada</p>
 	</div>
 </template>
 
 <script>
 export default {
-	
+	components: { },
+	directives: {
+		'destaque-local': {
+				bind(el, binding, vnode) {
+				const aplicarCor = cor => {
+					if(binding.arg === 'fundo') {
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
+				}
+				//el.style.backgroundColor = 'lightgreen'
+
+				let atraso = 0
+				if(binding.modifiers['atrasar']) atraso = 300
+ 
+				const cor1 = binding.value
+				const cor2 = 'purple'
+				let corAtual = cor1
+ 
+ 				setTimeout(() => {
+					if(binding.modifiers['alternar']) {
+						setInterval(() => {
+				  		}, 1000)
+					} 
+					aplicarCor(binding.value)  
+				}, atraso)
+			}
+		}
+	},
+	data() {
+		return {
+			cor: 'red'
+		}
+	}
 }
 </script>
 
--- curso-vue/diretivas/diretivas-exercicios/src/main.js
+++ curso-vue/diretivas/diretivas-exercicios/src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('destaque', {
+	bind(el, binding, vnode) {
+		//el.style.backgroundColor = 'lightgreen'
+		let atraso = 0
+
+		if(binding.modifiers['atrasar']) atraso = 300
+
+		setTimeout(() => {
+			if(binding.arg === 'fundo') {
+				el.style.backgroundColor = binding.value
+			} else {
+				el.style.color = binding.value
+			}
+		}, atraso)
+
+	}
+})
+
 new Vue({
 	render: h => h(App),
 }).$mount('#app')
